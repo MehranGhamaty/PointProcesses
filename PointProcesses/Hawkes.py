d1d43f154bf79647d44cc5bb03206e6c76a30644
MehranGhamaty <mnghamaty@gmail.com>
2021-01-14 21:03:08 -0800

    Update Hawkes.py

--- PointProcesses/Hawkes.py
+++ PointProcesses/Hawkes.py
@@ -5,7 +5,7 @@
     Assumes that there is a tensorflow session executing eagerly.
 
     Do not try to sample and perform gradient descent at the same time. 
-    The inference matrix has no restrictions and this would be a
+    
     Heterogenious Poisson Process. It just wasn't labeled.
 """
 from typing import List

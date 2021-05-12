32e8db92ee6e56a9468041665b8ee5c77fdff38a
MehranGhamaty <mnghamaty@gmail.com>
2021-05-12 15:56:44 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='PointProcesses', version='0.09a',
-      description="Implementation of various point processes",
-      author="Mehran N. Ghamaty",
+      description="Implementation of point processes",
+      author="Mehran Ghamaty",
       author_email="mnghamaty@gmail.com",
       packages=find_packages())
